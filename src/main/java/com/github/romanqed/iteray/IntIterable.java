36af97efb883a04cea9cacbcf79b95fe1a1dbccf
RomanQed <gbakaldin@gmail.com>
2025-07-28 16:03:35 +0300

    Add generic interfaces for primitive iterables

---
+++ src/main/java/com/github/romanqed/iteray/IntIterable.java
@@ -0,0 +1,36 @@
+package com.github.romanqed.iteray;
+
+import java.util.PrimitiveIterator;
+import java.util.Spliterator;
+import java.util.function.IntConsumer;
+
+/**
+ * A primitive iterable over {@code int} values, exposing {@link PrimitiveIterator.OfInt}
+ * and {@link Spliterator.OfInt}.
+ */
+public interface IntIterable extends PrimitiveIterable<Integer, IntConsumer, Spliterator.OfInt> {
+
+    /**
+     * Returns a {@link PrimitiveIterator.OfInt} over the {@code int} elements.
+     *
+     * @return a primitive int iterator
+     */
+    @Override
+    PrimitiveIterator.OfInt iterator();
+
+    /**
+     * Performs the given {@link IntConsumer} action for each {@code int} element.
+     *
+     * @param action the action to perform on each element
+     */
+    @Override
+    void forEach(IntConsumer action);
+
+    /**
+     * Returns a {@link Spliterator.OfInt} over the {@code int} elements.
+     *
+     * @return a primitive int spliterator
+     */
+    @Override
+    Spliterator.OfInt spliterator();
+}
